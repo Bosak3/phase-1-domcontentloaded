258628e6e761ca3d0ef47814811efba746fe0c95
Adam <bosak3@gmail.com>
2021-10-22 20:28:59 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+//Key Take Aways:
+            //DOMContentLoaded is the browsers bult-in way to indicate when a page's html is loaded into the DOM.
+            //It is not possible to manipulate HTML elements that haven't rendered yet, so trying to manipulate the DOM before the page fully loads can potentially lead to problems.
+            //We need to make sure to wait until after the DOMContentLoaded event is triggered to safely execute our code. 
+            //By creating an even listener, we can keep our code from immediately firing when index.js is loaded.
+
+
 // Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+   return text.textContent = "This is really cool!"
+})
+
+// console.log('I\'m first!')
